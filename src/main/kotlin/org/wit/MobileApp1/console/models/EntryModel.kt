4b27b79a5a2e2ff5d98853ac5e5840dd58a19046
emma murphy <emmamurphy-103@hotmail.com>
2021-10-21 12:51:13 +0100

    Add, Update,list and search

---
+++ src/main/kotlin/org/wit/MobileApp1/console/models/EntryModel.kt
@@ -0,0 +1,7 @@
+package org.wit.MobileApp1.console.models
+
+data class EntryModel(var id: Long = 0,
+                      var food: String = "",
+                      var date: String = "",
+                      var time: String = "",
+                      var kcal: String = "")
